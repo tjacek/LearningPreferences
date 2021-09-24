fe67a74b2444067fbdac1a4d7dbbbd76795edd32
tjacek <tjacek@agh.edu.pl>
2021-09-24 19:23:35 +0200

    votes

--- coombs.py
+++ coombs.py
@@ -1,5 +1,24 @@
+import numpy as np
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
+from matplotlib.collections import PatchCollection
+#from matplotlib.patches import Rectangle
+
+class Vote(object):
+    def __init__(self,prefs,t=7):
+        self.prefs=prefs
+        self.t=t
+
+    def show(self,ax):
+        ax.add_patch(plt.Rectangle((0.5, self.t), 6, 2, 
+        	ls="--", ec="c", fc="None",))
+        for pref_i in self.prefs:
+            pref_i.show(ax)
+
+    def get_bounds(self):
+        point=[[pref_i.x,pref_i.y] for pref_i in self.prefs]       
+        point=np.array(point)
+        return np.amax(point,axis=0)-np.amin(point,axis=0)
 
 class Pref(object):
     def __init__(self,x=3,y=3,name="A"):
@@ -9,14 +28,27 @@ class Pref(object):
 
     def show(self,ax):
         ax.text(self.x,self.y, self.name, fontsize=14,
-            bbox={'facecolor': 'red', 'alpha': 0.5, 'pad': 10})
+            bbox={'facecolor':'#0099FF', 'alpha': 0.5, 'pad': 10})
+
+def make_vote(x,y,names,t):
+    prefs=[ Pref(x+1.5*i,y,name_i) 
+             for i,name_i in enumerate( names)]	
+    return Vote(prefs,t)
 
 fig = plt.figure()
 ax = fig.add_subplot()
 fig.subplots_adjust(top=0.85)
 
-ax.axis([0, 10, 0, 10])
-pref=Pref()
-pref.show(ax)
+ax.axis([0, 10, -1, 10])
+plt.axis('off')
+
+vote=make_vote(1,8,["A","B","C","D"],7)
+vote.show(ax)
+vote=make_vote(1,5.5,["A","B","C","D"],4.5)
+vote.show(ax)
+vote=make_vote(1,3,["A","B","C","D"],2)
+vote.show(ax)
+vote=make_vote(1,0.5,["A","B","C","D"],-0.5)
+vote.show(ax)
 
 plt.show()
